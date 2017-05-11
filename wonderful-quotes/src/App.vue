91ac933a2c9c5d524a8213d071a3caf67144bcd4
noriyotcp <noriyo.akita@gmail.com>
2017-05-11 15:52:30 +0900

    [App.vue] Import QuoteGrid and pass quotes

--- wonderful-quotes/src/App.vue
+++ wonderful-quotes/src/App.vue
@@ -1,25 +1,12 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue'
+
 export default {
   name: 'app',
   data () {
@@ -29,6 +16,9 @@ export default {
       ],
       maxQuotes: 10
     }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
   }
 }
 </script>
