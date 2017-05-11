ca967b42de19bf961958cced1ed249f70d9577c2
noriyotcp <noriyo.akita@gmail.com>
2017-05-11 15:21:01 +0900

    [App.vue] Add quotes and maxQuotes to data

--- wonderful-quotes/src/App.vue
+++ wonderful-quotes/src/App.vue
@@ -24,7 +24,10 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      quotes: [
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
     }
   }
 }
