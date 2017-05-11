11ab5a963124563cb7839eee069d7eb8a77e9b87
noriyotcp <noriyo.akita@gmail.com>
2017-05-11 15:51:59 +0900

    Add Quote and QuoteGrid components

---
+++ wonderful-quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="row">
+    <app-quote v-for="quote, i in quotes" key="i">{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+
+export default {
+  props: ['quotes'],
+  components: {
+    appQuote: Quote
+  }
+}
+</script>
+
+<style scoped>
+  .panel-body {
+    font-family: 'Arizonia', cursive;
+    font-size: 24px;
+    color: #6e6e6e;
+  }
+
+  .quote {
+    cursor: pointer;
+  }
+
+  .quote:hover {
+    background-color: #ffe2e2;
+  }
+</style>
