89f049538cacedfa15416ec4b58d9a0dc3eeb0c2
noriyotcp <noriyo.akita@gmail.com>
2017-05-13 20:21:32 +0900

    Use v-text and v-html built-in directives

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -4,6 +4,8 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <h1>Built-in Directives</h1>
+          <p v-text="'Some Text'"></p>
+          <p v-html="'<strong>Some Text</strong>'"></p>
         </div>
       </div>
     </div>
