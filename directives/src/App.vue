5810aa6d9243ab36b34cbdfb1f20e8c4b3edc606
noriyotcp <noriyo.akita@gmail.com>
2017-05-13 22:22:57 +0900

    Bind mainColor, secondColor and delay and pass them to directives

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -10,7 +10,7 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <h1>Custom Directives</h1>
           <p v-highlight:background.delayed="'red'">Color This</p>
-          <p v-local-highlight:background.delayed.blink="'red'">Color This, too</p>
+          <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color This, too</p>
         </div>
       </div>
     </div>
@@ -28,8 +28,8 @@ export default {
           delay = 3000
         }
         if (binding.modifiers['blink']) {
-          let mainColor = binding.value
-          let secondColor = 'blue'
+          let mainColor = binding.value.mainColor
+          let secondColor = binding.value.secondColor
           let currentColor = mainColor
           setTimeout(() => {
             setInterval(() => {
@@ -40,14 +40,14 @@ export default {
               } else {
                 el.style.color = currentColor
               }
-            }, 1000)
+            }, binding.value.delay)
           }, delay)
         } else {
           setTimeout(() => {
             if (binding.arg == 'background') {
-              el.style.backgroundColor = binding.value
+              el.style.backgroundColor = binding.value.mainColor
             } else {
-              el.style.color = binding.value
+              el.style.color = binding.value.mainColor
             }
           }, delay)
         }
