64ad19abb72e60720dfac1eed0359e43bdc5af59
noriyotcp <noriyo.akita@gmail.com>
2017-05-13 20:40:28 +0900

    Check binding.modifiers['delayed']
    if it has 'delayed', backgroundColor or color after 3 seconds

--- directives/src/main.js
+++ directives/src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
-    if (binding.arg == 'background') {
-      el.style.backgroundColor = binding.value
-    } else {
-      el.style.color = binding.value
+    let delay = 0
+    if (binding.modifiers['delayed']) {
+      delay = 3000
     }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value
+      } else {
+        el.style.color = binding.value
+      }
+    }, delay)
   }
 })
 
