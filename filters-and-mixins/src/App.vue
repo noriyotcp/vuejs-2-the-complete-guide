c337aeb7ad09838a78b13dd6a1545a0fe2855b41
noriyotcp <noriyo.akita@gmail.com>
2017-05-13 23:23:23 +0900

    Enable to filter fruits

--- filters-and-mixins/src/App.vue
+++ filters-and-mixins/src/App.vue
@@ -5,6 +5,10 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <h1>Filters & Mixins</h1>
           <p>{{ text | toUpperCase | to-lowercase }}</p>
+          <input type="text" v-model="filterText">
+          <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+          </ul>
         </div>
       </div>
     </div>
@@ -12,16 +16,27 @@
 </template>
 
 <script>
+import { FRUITS } from './fruits'
+
 export default {
   name: 'app',
   data () {
     return {
-      text: 'Hello there!'
+      text: 'Hello there!',
+      fruits: FRUITS,
+      filterText: ''
     }
   },
   filters: {
     toUpperCase(value) {
       return value.toUpperCase()
+    }
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText)
+      });
     }
   }
 }
