7fa42520a52e657bda60ecf7c259369f96018403
noriyotcp <noriyo.akita@gmail.com>
2017-05-13 20:26:41 +0900

    Add custom directive 'highlight' and bind it to a paragraph with v- prefix

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -7,6 +7,10 @@
           <p v-text="'Some Text'"></p>
           <p v-html="'<strong>Some Text</strong>'"></p>
         </div>
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Custom Directives</h1>
+          <p v-highlight>Color This</p>
+        </div>
       </div>
     </div>
   </div>
