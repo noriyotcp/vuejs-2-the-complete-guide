bda2a76d1557436db311f4d75a92ee0c0caf984a
noriyotcp <noriyo.akita@gmail.com>
2017-05-13 22:37:48 +0900

    Register local filter called toUpperCase

--- filters-and-mixins/src/App.vue
+++ filters-and-mixins/src/App.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <h1>Filters & Mixins</h1>
-          <p>{{ text }}</p>
+          <p>{{ text | toUpperCase }}</p>
         </div>
       </div>
     </div>
@@ -17,6 +17,11 @@ export default {
   data () {
     return {
       text: 'Hello there!'
+    }
+  },
+  filters: {
+    toUpperCase(value) {
+      return value.toUpperCase()
     }
   }
 }
