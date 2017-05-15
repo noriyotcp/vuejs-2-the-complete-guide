7a63c12f90be40cc45a03bdbef66ed75c22dc6ff
noriyotcp <noriyo.akita@gmail.com>
2017-05-15 14:58:04 +0900

    Move appear attribute to 2nd alert

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -10,7 +10,7 @@
           <transition name="fade">
             <div class="alert alert-info" v-show="show">This is some Info</div>
           </transition>
-          <transition name="slide" type="animation">
+          <transition name="slide" type="animation" appear>
             <div class="alert alert-info" v-if="show">This is some Info</div>
           </transition>
           <transition name="slide" type="animation" appear>
