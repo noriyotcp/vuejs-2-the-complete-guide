6731b948264ae9abe29fb132909484d81fee7239
noriyotcp <noriyo.akita@gmail.com>
2017-05-15 13:36:13 +0900

    Show/Hide alert by clicking a button

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -4,6 +4,12 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <h1>Animations</h1>
+          <hr>
+          <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+          <br><br>
+          <transition>
+            <div class="alert alert-info" v-if="show">This is some Info</div>
+          </transition>
         </div>
       </div>
     </div>
@@ -15,7 +21,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      show: false
     }
   }
 }
