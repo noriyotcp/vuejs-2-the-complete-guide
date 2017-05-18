200ee0d85caa7d98a7cf78317febb4c74117b9a7
noriyotcp <noriyo.akita@gmail.com>
2017-05-18 16:26:26 +0900

    Introduce Bootstrap, clean up the code and import Question and Answer into App
    default component to render is Question

--- the-super-quiz/src/App.vue
+++ the-super-quiz/src/App.vue
@@ -1,31 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1 class="text-center">The Super Quiz</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <component :is="mode"></component>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Question from './components/Question.vue'
+import Answer from './components/Answer.vue'
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      mode: 'Question'
     }
+  },
+  components: {
+    Question,
+    Answer
   }
 }
 </script>
